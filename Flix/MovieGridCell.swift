800dbcad92ef3a989bd611765730ed883b9600bd
Jordan Sukhnandan <kirazuto7@gmail.com>
2022-02-18 22:17:36 -0500

    Flix part 2

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Jordan Sukhnandan on 2/18/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
